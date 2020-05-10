5e09af70c56c6a452d24c6819acbba2087904b2c
Adrian.Golebiewski <golebiewskiag@gmail.com>
2020-05-10 20:15:24 +0200

    init

---
+++ Projekty/TestAdrian/TestAdrian/Metody/Metody/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Metody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adria");
+        }
+    }
+}
